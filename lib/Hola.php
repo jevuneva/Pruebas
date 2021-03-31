047a2f45c0952f07780087822c24d2b6a59c027f
Jesus Negrete <jevuneva86@gmail.com>
2021-03-31 12:26:48 -0500

    Revert "hola usa la clase Holamundo"
    This reverts commit 282bdd48edd30926b03525ef64ceb0929c7a2226.

--- lib/Hola.php
+++ lib/Hola.php
@@ -1,6 +1,5 @@
 <? php
 // Jesus Negrete <Email: Jevuneva@hotmail.com>
 // El nombre por defecto es Mundo
-require('HolaMundo.php');
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
