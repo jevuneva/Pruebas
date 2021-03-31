282bdd48edd30926b03525ef64ceb0929c7a2226
Jesus Negrete <jevuneva86@gmail.com>
2021-03-31 12:24:01 -0500

    hola usa la clase Holamundo

--- lib/Hola.php
+++ lib/Hola.php
@@ -1,5 +1,6 @@
 <? php
 // Jesus Negrete <Email: Jevuneva@hotmail.com>
 // El nombre por defecto es Mundo
+require('HolaMundo.php');
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
